c67a27190d260c3f7a1128ca78f82d53db222368
Gurpreet Singh <g.singh6343@gmail.com>
2020-07-08 16:27:02 -0400

    Multiple changes; Added xorg config; Added rofi theme

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # Gurpreet's Dotfiles
 
 ---
+
+## TODO
+
+1. Clean up and add themed jupyter custom files
+2. Clean up latex style files
+3. Config ranger
+4. Config grub
---
+++ python/jupyter/install.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/sh
+
+pip install jupyter ipywidgets RISE
+jupyter nbextension install https://github.com/drillan/jupyter-black/archive/master.zip --user
+
+jupyter nbextension enable --py widgetsnbextension
+jupyter nbextension enable --py rise
+jupyter nbextension enable jupyter-black-master/jupyter-black
